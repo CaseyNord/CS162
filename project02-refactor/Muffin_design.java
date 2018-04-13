6bdcdda3e3117b08a754b7070907be04b769c035
Casey Nord <nordcw@gmail.com>
2018-04-12 18:25:53 -0700

    Added ability to read from file

--- project02-refactor/Muffin_design.java
+++ project02-refactor/Muffin_design.java
@@ -10,17 +10,22 @@
 //				check if you can quit the program. check if the loop brings you back to the menu.
 // *********************************************************************************************
 import java.util.Scanner;
+import java.io.*;
+import java.util.stream.*;
 
 public class Muffin_design
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		Scanner scan = new Scanner (System.in);
+
+		int Amt = 0;
+		String Topp = "", size = "";
 
 		char Ans = 'Y';
 		while (Ans == 'Y' || Ans == 'y')
 		{
-			System.out.println("Menu: \t Press 1 to make a batch of muffins \n\t Press 2 to quit the program \n");
+			System.out.println("Menu: \t Press 1 to make a batch of muffins \n\t Press 2 to read from file \n\t Press 3 to quit the program \n");
 			String input = scan.next();
 			int choice;
 			try
@@ -31,12 +36,12 @@ public class Muffin_design
 			{
 				choice = 0;
 			}
-			
+
 			if (choice == 1)
 			{
 				System.out.print("\nToday we are baking muffins! \n\n");
 				System.out.println("How many muffins would you like to bake today? ");
-				int Amt = scan.nextInt();
+				Amt = scan.nextInt();
 					while (Amt < 1)
 					{	
 						System.out.println("You entered a number out of range, please enter a positive number of muffins you want to bake ");
@@ -45,7 +50,7 @@ public class Muffin_design
 					}
 
 				System.out.println("Which of these toppings would you like? Raspberry, Blueberry, Chocolate-Chip");
-				String Topp = scan.next();
+				Topp = scan.next();
 				Topp = Topp.toLowerCase();
 					while (!Topp.equals("raspberry") && !Topp.equals("blueberry") && !Topp.equals("chocolate-chip"))
 					{	
@@ -55,7 +60,7 @@ public class Muffin_design
 					}
 
 				System.out.println("What size do you want your muffins to be? Small, Medium, Large");
-				String size = scan.next();
+				size = scan.next();
 				size = size.toLowerCase();
 					while (!size.equals("small") && !size.equals("medium") && !size.equals("large"))
 					{	
@@ -80,6 +85,39 @@ public class Muffin_design
 				Ans = Word.charAt(0);
 			}
 			else if (choice == 2)
+			{
+				Scanner fileScan, lineScan;
+				fileScan = new Scanner(new File("Muffins.txt"));
+				String ingredients = fileScan.nextLine();
+				lineScan = new Scanner(ingredients);
+				lineScan.useDelimiter(" ");
+
+				while (lineScan.hasNext())
+				{
+					Amt = lineScan.nextInt();
+					Topp = lineScan.next();
+					size = lineScan.next();
+
+				}
+				Topp = Topp.toLowerCase();
+				size = size.toLowerCase();
+				
+				Batch_Of_Muffins Batch_1 = new Batch_Of_Muffins (Amt, Topp, size);
+				System.out.println("\n" + Batch_1 + "\n\n");
+
+				System.out.println("******************************************************************************************************************");
+
+				System.out.println("For " + Amt + " muffins, you need " + Batch_1.Amount_Topping(Amt) + " " + Topp +"s \n");
+				System.out.println("You need to cook your batch of muffins at " + Batch_1.Temperature(size) + " fahrenheit.");
+
+				System.out.println("******************************************************************************************************************");
+
+				System.out.println("\n\nDo you want to go back to the menu? (Y/N) \n ");
+				String Word = scan.next();
+				Word = Word.toUpperCase();
+				Ans = Word.charAt(0);
+			}
+			else if (choice == 3)
 				System.exit(2);
 			else
 				System.out.println("Please enter a valid option.");
